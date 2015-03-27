cefa95e927e635419fc6058cbe339d1129bd262a
M. Holender <mosheemes@gmail.com>
2015-03-27 10:13:52 -0400

    put global variable numberOfVowels inside function

--- vowelcount.js
+++ vowelcount.js
@@ -1,21 +1,27 @@
 // Challenge: Build a function vowelCount() which takes a string and returns 
 // the number of vowels (AEIOUY).
 
-var numberOfVowels, phrase;
+var phrase;
 
 function isVowel(value) {
 	value = value.toLowerCase();
+	
 	if (value==="a" || value==="e" || value==="i" || value==="o" || value==="u" || value==="y") {
-		numberOfVowels += 1;
-	}
+		return 1;
+	} else { return 0; }
+	
 }
 
 function vowelCount(string) {
-	var testArr = string.split('');
-	numberOfVowels = 0;
+	var i, numberOfVowels = 0, testArr = string.split('');
 	
-	testArr.forEach(isVowel);
+	// It wasn't immediately obvious to me how to accomplish this with .forEach
+	// so I switched to using a for statement.
 	
+	for (i=0; i<testArr.length; i++) {
+		numberOfVowels += isVowel(testArr[i]);
+	}
+		
 	return numberOfVowels;
 }
 
